cb3a849bcb36b8fb98dba922fcdf1d337ebece10
Random <155200367+lidth@users.noreply.github.com>
2024-02-22 18:57:09 -0300

    carrosel made

---
+++ script.js
@@ -0,0 +1,36 @@
+const images = [
+    'assets/images/products/notebook/notebook-01.jpg',
+    'assets/images/products/notebook/notebook-02.jpg',
+    'assets/images/products/notebook/notebook-03.jpg',
+    'assets/images/products/notebook/notebook-04.jpg'
+]
+
+let indiceAtual = 0;
+let imagem = document.querySelector('img.productimg')
+
+let rightarrow = document.getElementById('rightarrow')
+let leftarrow = document.getElementById('leftarrow')
+
+function trocarimagem(){
+    imagem.src = images[indiceAtual]
+}
+
+rightarrow.addEventListener("click", function(){
+    if(indiceAtual === 3){
+    indiceAtual = 0
+    console.log(indiceAtual)
+    }else{
+        indiceAtual += 1;
+        console.log(indiceAtual)
+    }
+    trocarimagem();
+})
+
+leftarrow.addEventListener("click", function(){
+    if(indiceAtual == 0){
+        indiceAtual = 3
+    }else{
+    indiceAtual = indiceAtual - 1;
+    }
+    trocarimagem()
+})
